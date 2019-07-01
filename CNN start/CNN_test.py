24e3b005942bac0db5b31cb1d26508b4023b2118
MaximilianDu <30485928+MaximilianDu@users.noreply.github.com>
2019-07-01 10:31:30 -0400

    yeeeeeeeet

--- CNN start/CNN_test.py
+++ CNN start/CNN_test.py
@@ -4,54 +4,69 @@ import numpy as np
 
 
 
-def make_weights(shape):
-    distribution = tf.truncated_normal(shape, stddev =  0.1)
-    var = tf.Variable(distribution)
-    return var
+def make_weights(shape, name):
+    weight_name = name + "_Weight"
+    with tf.name_scope("Weights"):
+        distribution = tf.truncated_normal(shape, stddev =  0.1, name = weight_name)
+        var = tf.Variable(distribution)
+        tf.summary.histogram(weight_name, var)
+        return var
 
-def make_bias(shape):
-    distribution = tf.constant(0.1, shape = shape)
-    var = tf.Variable(distribution)
-    return var
+def make_bias(shape, name):
+    bias_name = name + "_Bias"
+    with tf.name_scope("Biases"):
+        distribution = tf.constant(0.1, shape = shape, name = bias_name)
+        var = tf.Variable(distribution)
+        tf.summary.histogram(bias_name, var)
+        return var
 
-def conv2d(input, filter):
-    output = tf.nn.conv2d(input, filter, strides = [1,1,1,1], padding = 'SAME') #this essentially does the filter operation keeping the dimensions the same
-    return output
+def conv2d(input, filter, name):
+    conv_name = name +"_Conv"
+    with tf.name_scope("Conv"):
+        output = tf.nn.conv2d(input, filter, strides = [1,1,1,1], padding = 'SAME', name = conv_name) #this essentially does the filter operation keeping the dimensions the same
+        return output
 
-def max_pool(input):
-    output = tf.nn.max_pool(input, ksize = [1,2,2,1], strides = [1,2,2,1], padding = 'SAME') #ksize is the window size, and it just pools every 4 grid into 1 (need to work out why later)
-    return output
+def max_pool(input, name):
+    pool_name = name + "_Pool"
+    with tf.name_scope("Pool"):
+        output = tf.nn.max_pool(input, ksize = [1,2,2,1], strides = [1,2,2,1], padding = 'SAME', name = pool_name) #ksize is the window size, and it just pools every 4 grid into 1 (need to work out why later)
+        return output
 
-def convolutional_layer(input, shape):
-    W = make_weights(shape)
-    b = make_bias([shape[3]])
-    return tf.nn.relu(conv2d(input, W) + b)
+def convolutional_layer(input, shape, name):
+    W = make_weights(shape, name)
+    b = make_bias([shape[3]], name)
+    return tf.nn.relu(conv2d(input, W, name) + b)
 
-def fully_connected(input, end_size):
+def fully_connected(input, end_size, name):
     in_size = int(input.get_shape()[1])
-    W = make_weights([in_size, end_size])
-    b = make_bias([end_size])
+    W = make_weights([in_size, end_size], name)
+    b = make_bias([end_size], name)
     return(tf.matmul(input, W) + b)
 
 x = tf.placeholder(tf.float32, shape = [None, 32,32,3])
 truth = tf.placeholder(tf.float32, shape = [None, 10])
 hold_prob = tf.placeholder(tf.float32)
 
-conv_1 = convolutional_layer(x, shape = [4,4,3,32]) # 4 and 4 is the window, 3 is the color channels and 32 is number of output layers (filters)
-conv_1_pooled = max_pool(conv_1)
-conv_2 = convolutional_layer(conv_1_pooled, shape = [4,4,32,64])
-conv_2_pooled = max_pool(conv_2)
+conv_1 = convolutional_layer(x, shape = [4,4,3,32], name = "Layer_1") # 4 and 4 is the window, 3 is the color channels and 32 is number of output layers (filters)
+conv_1_pooled = max_pool(conv_1, name = "Layer_1")
+conv_2 = convolutional_layer(conv_1_pooled, shape = [4,4,32,64], name = "Layer_2")
+conv_2_pooled = max_pool(conv_2, name = "Layer_2")
 
 
-flattened = tf.reshape(conv_2_pooled, [-1, 8*8*64])
-fc_1 = fully_connected(flattened, 1024)
+flattened = tf.reshape(conv_2_pooled, [-1, 8*8*64], name = "Flatten")
+fc_1 = fully_connected(flattened, 1024, name = "Fully_Connected_Layer_1")
 dropout_1 = tf.nn.dropout(fc_1, keep_prob = hold_prob)
-prediction = fully_connected(dropout_1, 10)
+prediction = fully_connected(dropout_1, 10, name = "Fully_Connected_Layer_2")
 
-loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels = truth, logits = prediction))
+loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels = truth, logits = prediction, name = "Softmax_loss"))
 
-optimizer = tf.train.AdamOptimizer(learning_rate = 0.001)
+optimizer = tf.train.AdamOptimizer(learning_rate = 0.001, name = "Optimizer")
 train = optimizer.minimize(loss)
+
+with tf.name_scope("Saver"):
+    tf.summary.scalar("Loss", loss)
+    summary_op = tf.summary.merge_all()
+    saver = tf.train.Saver(max_to_keep=9)
 
 init = tf.global_variables_initializer()
 
@@ -61,15 +76,22 @@ with tf.Session() as sess:
     datafeeder = Prep()
 
     for i in range(500):
-        data, label = datafeeder.nextBatchTrain(100)
+        data, label = datafeeder.nextBatchTrain(1)
         prediction_, loss_, _ = sess.run([prediction, loss, train], feed_dict = {x:data, truth:label, hold_prob:1})
         print("This is the loss: {}".format(loss_))
         if i % 50 == 0:
             print("Finished epoch: {}".format(i))
         if i % 100 == 0:
+            saver.save(sess, "Graphs_and_Results/", global_step=epoch)
             data, label = datafeeder.nextBatchTest()
+            print(np.shape(data))
+            print(np.shape(label))
+            input()
             correct = 0
             prediction_ = sess.run(prediction, feed_dict = {x:data, truth:label, hold_prob:1})
+            print(prediction_)
+            print(label)
+            input()
             for k, l in zip(prediction_, label):
                 if(tf.argmax(k) == tf.argmax(l)):
                     correct += 1
--- CNN start/DataProcess.py
+++ CNN start/DataProcess.py
@@ -28,13 +28,23 @@ class Prep():
         files = "test_batch"
         bigdata = list()
         bigdata.append(self.unpickle("data/" + files))
-
         batch = np.vstack([d[b'data'] for d in bigdata])
         training_length = len(batch)
         batch = batch.reshape(training_length, 3,32,32).transpose(0,2,3,1)
         labels = np.vstack([d[b'labels'] for d in bigdata])
         O_H = self.oneHot(labels)
         return batch, O_H
+
+    def unzip_test_small(self):
+        files = "test_batch"
+        bigdata = list()
+        bigdata.append(self.unpickle("data/" + files))
+        batch = np.vstack([d[b'data'] for d in bigdata])
+        training_length = len(batch)
+        batch = batch.reshape(training_length, 3,32,32).transpose(0,2,3,1)
+        labels = np.vstack([d[b'labels'] for d in bigdata])
+        O_H = self.oneHot(labels)
+        return batch[0:100], O_H[0:100]
 
 
     def getkey(self, data):
@@ -53,6 +63,7 @@ class Prep():
     #postconditions: outputs a 2d array with of 1-hot encodings, with the 1st index being for image
     def oneHot(self, labels):
         dimensions = len(labels)
+        print(dimensions)
         carrier = np.zeros([dimensions, 10])
         for k, l in zip(carrier, labels):
             k[l] = 1
@@ -67,5 +78,5 @@ class Prep():
         return batch, O_H
 
     def nextBatchTest(self):
-        batch, O_H = self.unzip_training()
+        batch, O_H = self.unzip_test_small()
         return batch, O_H
