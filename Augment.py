0dd4de7af7657e3ebc67618fe23ea5264197e3a8
MaximilianDu <30485928+MaximilianDu@users.noreply.github.com>
2019-07-08 16:37:25 -0400

    oh boy

--- Augment.py
+++ Augment.py
@@ -2,23 +2,58 @@ from Utility import Utility
 import csv
 import numpy as np
 import random
-
+from collections import Counter
 
 tool = Utility()
 
 basepath = "DATASET_Motion/"
 savepath = "DATASET_augmented/"
-k = open(basepath + "labels.csv", 'r')
+k = open("dom_labels.csv", 'r')
 labels = list(csv.reader(k))
+
+#this just find the frequency distribution of the csv
+big_list = list()
+for k in labels:
+    big_list.extend(k)
+counted = Counter(big_list)
+
+#these calculate individual values of conversions (aka how many times you need to augment)
+conversion_dict = {} #these hold the mappings of all non-zero values
+for i in range(1, 83):
+    occurence = counted[str(i)]
+    if(occurence != 0):
+        scaling_factor = int(round(100/occurence))
+        conversion_dict[str(i)] = scaling_factor
+
+
 #we shrink, crop, and augment the dataset
+#this is for dom, only
+
+
 for i in range(len(labels)):
     print("I'm on image: {}".format(i))
     img_path = basepath + str(i) + ".jpg"
     matrix = tool.load_image_to_mat(img_path)
     matrix = [k[80:560] for k in matrix]
     matrix = np.asarray(matrix)
-
     matrix = tool.resize_image(matrix, 48, 48, "L")
+
+    #this section simply takes the known augmentation factor and takes an average (because there are more than one
+    #per row sometimes
+    aug_list = list()
+    for k in labels[i]:
+        aug_list.append(conversion_dict[k])
+    aug_num = int(round(np.sum(aug_list)/len(aug_list)))
+    aug_num = aug_num -1 #this is the EXISTING one
+    print(aug_num)
+    print(labels[i])
+    print(conversion_dict)
+    raise Exception
+    ################
+    #here is some programming debauchery
+
+
+
     tool.save_image(matrix=matrix, path=savepath + str(i) + "_0.jpg", type="L")
 
     print("\tFlipping left-right")
