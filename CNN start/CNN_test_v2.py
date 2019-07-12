d56feaca06529f02d2681adf3298eb4023d10b6a
MaximilianDu <30485928+MaximilianDu@users.noreply.github.com>
2019-07-12 09:26:34 -0400

    split into layers

--- CNN start/CNN_test_v2.py
+++ CNN start/CNN_test_v2.py
@@ -5,83 +5,161 @@ import csv
 import os
 hold_prob = 0.6
 
-class CustomLayer(tf.keras.layers.Layer): #this uses a keras layer structure but with a custom layer
+class Convolve(tf.keras.layers.Layer): #this uses a keras layer structure but with a custom layer
     def __init__(self, *args, **kwargs):
-        super(CustomLayer, self).__init__(*args, **kwargs)
+        super(Convolve, self).__init__(*args, **kwargs)
 
     def build(self, input_shape):
         self.w_conv_1 = self.add_weight(
-            shape=[4,4,3,32],
+            shape=input_shape,
             dtype=tf.float32,
             initializer=tf.keras.initializers.TruncatedNormal(),
             regularizer=tf.keras.regularizers.l2(0.02),
             trainable=True,
-            name="a")
+            name="Convolve")
 
         self.b_conv_1 = self.add_weight(
-            shape=[32],
+            shape=input_shape[3],
             dtype=tf.float32,
             initializer=tf.keras.initializers.zeros(),
             regularizer=tf.keras.regularizers.l2(0.02),
             trainable=True,
-            name = "b")
-        self.w_conv_2 = self.add_weight(
-            shape=[4, 4, 32, 64],
-            dtype=tf.float32,
-            initializer=tf.keras.initializers.TruncatedNormal(),
-            regularizer=tf.keras.regularizers.l2(0.02),
-            trainable=True,
-            name = "c")
-        self.b_conv_2 = self.add_weight(
-            shape=[64],
-            dtype=tf.float32,
-            initializer=tf.keras.initializers.zeros(),
-            regularizer=tf.keras.regularizers.l2(0.02),
-            trainable=True,
-            name = "d")
-        self.w_conv_3 = self.add_weight(
-            shape=[4, 4, 64, 128],
-            dtype=tf.float32,
-            initializer=tf.keras.initializers.TruncatedNormal(),
-            regularizer=tf.keras.regularizers.l2(0.02),
-            trainable=True,
-            name = "e")
-        self.b_conv_3 = self.add_weight(
-            shape=[128],
-            dtype=tf.float32,
-            initializer=tf.keras.initializers.zeros(),
-            regularizer=tf.keras.regularizers.l2(0.02),
-            trainable=True,
-            name = "f")
-        self.w_fc_1 = self.add_weight(
-            shape=[4*4*128, 1024],
-            dtype=tf.float32,
-            initializer=tf.keras.initializers.TruncatedNormal(),
-            regularizer=tf.keras.regularizers.l2(0.02),
-            trainable=True,
-            name = "g")
-        self.b_fc_1 = self.add_weight(
-            shape=[1024],
-            dtype=tf.float32,
-            initializer=tf.keras.initializers.zeros(),
-            regularizer=tf.keras.regularizers.l2(0.02),
-            trainable=True,
-            name = "i")
-        self.w_fc_2  = self.add_weight(
-            shape=[1024,10],
-            dtype=tf.float32,
-            initializer=tf.keras.initializers.ones(),
-            regularizer=tf.keras.regularizers.l2(0.02),
-            trainable=True,
-            name = "j")
-        self.b_fc_2 = self.add_weight(
-            shape=[10],
-            dtype=tf.float32,
-            initializer=tf.keras.initializers.zeros(),
-            regularizer=tf.keras.regularizers.l2(0.02),
-            trainable=True,
-            name = "k")
+            name = "Convolve_Bias")
 
+    @tf.function
+    def call(self, input, training=None):
+        conv_1 = tf.nn.relu(tf.nn.conv2d(input, self.w_conv_1, strides=[1, 1, 1, 1], padding='SAME', name="conv_1"))
+        conv_1 = conv_1 + self.b_conv_1
+        pooled_1 = tf.nn.max_pool(conv_1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME', name="pool_1")
+        return pooled_1
+        
+        
+class Flatten(tf.keras.layers.Layer):
+    def __init__(self, *args, **kwargs):
+        super(Flatten, self).__init__(*args, **kwargs)
+
+    def build(self, input_shape):
+        self.size = input_shape
+        pass
+
+    @tf.function
+    def call(self, input, training=None):
+        flattened = tf.reshape(input, self.size, name="Flatten")
+        return flattened
+
+class Softmax(tf.keras.layers.Layer):  # this uses a keras layer structure but with a custom layer
+    def __init__(self, *args, **kwargs):
+        super(Softmax, self).__init__(*args, **kwargs)
+
+    def build(self, input_shape):
+        pass
+
+    @tf.function
+    def call(self, input, training=None):
+        prediction = tf.nn.softmax(input)
+        return prediction
+
+class FC(tf.keras.layers.Layer):  # this uses a keras layer structure but with a custom layer
+    def __init__(self, *args, **kwargs):
+        super(FC, self).__init__(*args, **kwargs)
+
+    def build(self, input_shape):
+        self.w_fc_1 = self.add_weight(
+            shape=input_shape,
+            dtype=tf.float32,
+            initializer=tf.keras.initializers.TruncatedNormal(),
+            regularizer=tf.keras.regularizers.l2(0.02),
+            trainable=True,
+            name="Fully_Connected_Weight")
+        self.b_fc_1 = self.add_weight(
+            shape=input_shape[1],
+            dtype=tf.float32,
+            initializer=tf.keras.initializers.zeros(),
+            regularizer=tf.keras.regularizers.l2(0.02),
+            trainable=True,
+            name="Fully_Connected_Bias")
+
+    @tf.function
+    def call(self, input, training=None):
+        fc_1 = tf.matmul(input, self.w_fc_1) + self.b_fc_1
+        if training:
+            self.fc_1_d = tf.nn.dropout(fc_1, rate=1 - hold_prob)
+        else:
+            self.fc_1_d = hold_prob * self.fc_1_d
+        return self.fc_1_d
+
+
+    self.w_conv_1 = self.add_weight(
+        shape=[4, 4, 3, 32],
+        dtype=tf.float32,
+        initializer=tf.keras.initializers.TruncatedNormal(),
+        regularizer=tf.keras.regularizers.l2(0.02),
+        trainable=True,
+        name="a")
+
+    self.b_conv_1 = self.add_weight(
+        shape=[32],
+        dtype=tf.float32,
+        initializer=tf.keras.initializers.zeros(),
+        regularizer=tf.keras.regularizers.l2(0.02),
+        trainable=True,
+        name="b")
+    self.w_conv_2 = self.add_weight(
+        shape=[4, 4, 32, 64],
+        dtype=tf.float32,
+        initializer=tf.keras.initializers.TruncatedNormal(),
+        regularizer=tf.keras.regularizers.l2(0.02),
+        trainable=True,
+        name="c")
+    self.b_conv_2 = self.add_weight(
+        shape=[64],
+        dtype=tf.float32,
+        initializer=tf.keras.initializers.zeros(),
+        regularizer=tf.keras.regularizers.l2(0.02),
+        trainable=True,
+        name="d")
+    self.w_conv_3 = self.add_weight(
+        shape=[4, 4, 64, 128],
+        dtype=tf.float32,
+        initializer=tf.keras.initializers.TruncatedNormal(),
+        regularizer=tf.keras.regularizers.l2(0.02),
+        trainable=True,
+        name="e")
+    self.b_conv_3 = self.add_weight(
+        shape=[128],
+        dtype=tf.float32,
+        initializer=tf.keras.initializers.zeros(),
+        regularizer=tf.keras.regularizers.l2(0.02),
+        trainable=True,
+        name="f")
+    self.w_fc_1 = self.add_weight(
+        shape=[4 * 4 * 128, 1024],
+        dtype=tf.float32,
+        initializer=tf.keras.initializers.TruncatedNormal(),
+        regularizer=tf.keras.regularizers.l2(0.02),
+        trainable=True,
+        name="g")
+    self.b_fc_1 = self.add_weight(
+        shape=[1024],
+        dtype=tf.float32,
+        initializer=tf.keras.initializers.zeros(),
+        regularizer=tf.keras.regularizers.l2(0.02),
+        trainable=True,
+        name="i")
+    self.w_fc_2 = self.add_weight(
+        shape=[1024, 10],
+        dtype=tf.float32,
+        initializer=tf.keras.initializers.ones(),
+        regularizer=tf.keras.regularizers.l2(0.02),
+        trainable=True,
+        name="j")
+    self.b_fc_2 = self.add_weight(
+        shape=[10],
+        dtype=tf.float32,
+        initializer=tf.keras.initializers.zeros(),
+        regularizer=tf.keras.regularizers.l2(0.02),
+        trainable=True,
+        name="k")
     @tf.function
     def call(self, input, training = None):
         conv_1 = tf.nn.relu(tf.nn.conv2d(input, self.w_conv_1, strides = [1,1,1,1], padding = 'SAME', name = "conv_1"))
@@ -130,6 +208,7 @@ def Conf_mat():
     loss_function = tf.keras.losses.CategoricalCrossentropy(from_logits=True)
     model = tf.keras.Sequential([CustomLayer()])
     model.compile(optimizer=optimizer, loss=loss_function)
+    model.build([1])
     model.load_weights("Graphs_and_Results/best_weights.h5")
     datafeeder = Prep()
 
