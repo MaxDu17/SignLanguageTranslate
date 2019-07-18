2524953ef8b96088dbc4d4ee35319ca44de26d01
MaximilianDu <30485928+MaximilianDu@users.noreply.github.com>
2019-07-18 10:52:59 -0400

    next

--- Augment.py
+++ Augment.py
@@ -19,8 +19,8 @@ def crop(matrix):
 
 
 #VERY IMPORTANT NOTE--THIS IS ONLY FOR DOMINANT HAND
-basepath = "../LINKED/Storage/Data/experimental/edge_history/"
-savepath = "../LINKED/Storage/Data/experimental/augmented_edge_history/"
+basepath = "../LINKED/Storage/Data/experimental/history/"
+savepath = "../LINKED/Storage/Data/experimental/augmented_history/"
 k = open("../LINKED/Storage/Data/experimental/dom_labels.csv", 'r')
 labels = list(csv.reader(k))
 
