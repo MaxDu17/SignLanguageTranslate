113fb19df02e342c2f2f17e1b5739c52f7640f57
MaximilianDu <30485928+MaximilianDu@users.noreply.github.com>
2019-07-22 09:18:29 -0400

    trying this

--- MotionEnergyModel.py
+++ MotionEnergyModel.py
@@ -127,7 +127,6 @@ class Model():
         self.fc_1.build()
     @tf.function
     def call(self, input):
-        tf.summary.trace_on()
         x = self.cnn_1.call(input)
         x = self.cnn_2.call(x)
         x = self.flat.call(x)
@@ -162,6 +161,7 @@ def Big_Train():
     print("Making model")
     model = Model()
     model.build_model()
+    tf.summary.trace_on(graph=True, profiler=True)
 
     for epoch in range(401):
         data, label = datafeeder.nextBatchTrain_dom(150)
@@ -179,7 +179,7 @@ def Big_Train():
                 with summary_writer.as_default(): #this is not working rn but I will fix it
                     tf.summary.scalar(name = "Loss", data = pred_loss, step = epoch)
                     tf.summary.scalar(name = "Accuracy", data = accuracy(predictions, label), step = epoch)
-                    tf.summary.trace_export(name = "Graph")
+                    tf.summary.trace_export(name = "Graph", step = epoch, profiler_outdir="Graphs_and_Results")
                     for var in big_list:
                         name = str(var.name)
                         tf.summary.histogram(name = "Variable_" + name, data = var, step = epoch)
