c60f7441173494e436417495d575b4af54efb9f0
MaximilianDu <30485928+MaximilianDu@users.noreply.github.com>
2019-07-26 14:15:03 -0400

    confused much

--- DualModel.py
+++ DualModel.py
@@ -63,6 +63,7 @@ class Model():
     @tf.function
     def call(self, input):
         print(np.shape(input))#there's some fishy business going on here
+        raise Exception
         x= self.cnn_1_e.call(input[0])
         l2 = self.cnn_1_e.l2loss()
         x = self.cnn_2_e.call(x)
@@ -138,8 +139,6 @@ def Big_Train():
 
     for epoch in range(1001):
         data, label = datafeeder.nextBatchTrain_dom(150)
-        print(np.shape(data))
-        raise Exception
         with tf.GradientTape() as tape:
             predictions, l2_loss = model.call(data) #this is the big call
 
