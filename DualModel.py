137774c5afd9acd02954fbedd4e11f15e63a0b62
MaximilianDu <30485928+MaximilianDu@users.noreply.github.com>
2019-07-26 15:17:29 -0400

    whoops

--- DualModel.py
+++ DualModel.py
@@ -103,7 +103,7 @@ def record_error(data, labels, pred):
     print(np.shape(data))
     print(np.shape(labels))
     print(np.shape(pred))
-    assert len(data) == len(pred), "your data and prediction don't match"
+    assert len(data[0]) == len(pred), "your data and prediction don't match"
     assert len(pred) == len(labels), "your prediction and labels don't match"
 
     wrong = list()
