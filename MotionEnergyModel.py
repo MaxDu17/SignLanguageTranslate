bd5b6ce263ce20a613cc1f8f00286524ea7c421f
MaximilianDu <30485928+MaximilianDu@users.noreply.github.com>
2019-07-29 11:32:28 -0400

    debugging why this might happen

--- MotionEnergyModel.py
+++ MotionEnergyModel.py
@@ -50,7 +50,7 @@ class Model():
 
     @tf.function
     def call(self, input):
-        print(np.shape(input))
+        print("I am in calling {}".format(np.shape(input)))
         x= self.cnn_1.call(input)
         l2 = self.cnn_1.l2loss()
         x = self.cnn_2.call(x)
