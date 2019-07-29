7ebd706a897a39686edadb6fedc7dae1fc9b6953
MaximilianDu <30485928+MaximilianDu@users.noreply.github.com>
2019-07-29 11:01:46 -0400

    dammit

--- DualModel.py
+++ DualModel.py
@@ -316,7 +316,7 @@ def Test():
 
     for i in range(len(right)):
         print("Saving right image {}".format(i))
-        carrier = np.reshape(wrong[i], [100, 100])
+        carrier = np.reshape(right[i], [100, 100])
         util.save_image(255 * carrier, "Graphs_and_Results/dual/" + version + "/right/" + str(i) + ".jpg", "L")
 
 def main():
